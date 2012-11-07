7509d0837a6b3280e5411a031fe4a339c7cb179d
3delStahl <f.giller@sky-lab.de>
2012-11-07 22:34:16 +0100

    Update backend/index.php
    autolaod disabled

--- backend/index.php
+++ backend/index.php
@@ -1,13 +1,12 @@
 <?php
-<<<<<<< HEAD
+/*
 function __autoload($class_name)
 {
 	include(dirname(dirname(__FILE__))."/static/class.".$class_name.".php");
 }
-
-=======
+*/
 require_once($_SERVER["DOCUMENT_ROOT"]."/coffee/static/class.database.php");
->>>>>>> database & user class update
+
 require_once("classes/class.user.php");
 
 $output = "<h1>Administration</h1>";
