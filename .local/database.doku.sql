a5b6389a7092ab87a6f65521478c927dc532de4b
Leon Bergmann <leon.bergmann@sky-lab.de>
2012-11-12 12:11:22 +0100

    convertet old database.doku.php
    convertet old database.doku.php from InnoDB to MYISAM

--- .local/database.doku.sql
+++ .local/database.doku.sql
@@ -1,47 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 3.5.2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Erstellungszeit: 12. Nov 2012 um 11:59
--- Server Version: 5.5.25a
--- PHP-Version: 5.4.4
+CREATE DATABASE IF NOT EXISTS `doku`; 
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Datenbank: `doku`
---
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `changelog`
---
-
-CREATE TABLE IF NOT EXISTS `changelog` (
+CREATE TABLE IF NOT EXISTS `doku`.`changelog` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
   `version` int(8) NOT NULL,
   `action` varchar(500) NOT NULL,
   `autor` varchar(500) NOT NULL,
   `date` int(13) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE = MYISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `classes`
---
-
-CREATE TABLE IF NOT EXISTS `classes` (
+CREATE TABLE IF NOT EXISTS `doku`.`classes` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
   `functions-id` int(8) NOT NULL,
   `name` varchar(500) NOT NULL,
@@ -50,15 +18,10 @@ CREATE TABLE IF NOT EXISTS `classes` (
   `version` int(8) NOT NULL,
   `date` int(13) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE = MYISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
 
---
--- Tabellenstruktur für Tabelle `functions`
---
-
-CREATE TABLE IF NOT EXISTS `functions` (
+CREATE TABLE IF NOT EXISTS `doku`.`functions` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) NOT NULL,
   `changelog-id` int(8) NOT NULL,
@@ -66,8 +29,4 @@ CREATE TABLE IF NOT EXISTS `functions` (
   `kurz-beschreibung` varchar(500) NOT NULL,
   `return-wert` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+) ENGINE = MYISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
