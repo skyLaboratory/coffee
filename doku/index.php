567c668b56e4825b37f22170cdd843a3f0ec039e
Patrick Kellenter <patrick.kellenter@gmx.de>
2012-11-12 11:40:56 +0100

    HTML 5

--- doku/index.php
+++ doku/index.php
@@ -4,8 +4,9 @@
 
 //Herstellen Datenbankverbindung
 
+echo "<!DOCTYPE html><html><head></head><body>";
 echo "<h1>Dokumentation - Coffee</h1><br />";
 echo "<select><option>Neue class anlegen</option></select><input type='submit' value='>>>'/><br /><br />";
 echo "<select><option>Neue function anlegen</option></select><input type='submit' value='>>>'/><br /><br />";
-
+echo "</body></html>";
 ?>
