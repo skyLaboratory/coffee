be674c198a624e36dd5cee098f83eaba2f5ee0a6
Patrick Kellenter <patrick.kellenter@gmx.de>
2012-11-12 11:50:14 +0100

    Formulare angelegt doku/index.php

--- doku/index.php
+++ doku/index.php
@@ -6,7 +6,12 @@
 
 echo "<!DOCTYPE html><html><head></head><body>";
 echo "<h1>Dokumentation - Coffee</h1><br />";
-echo "<select><option>Neue class anlegen</option></select><input type='submit' value='>>>'/><br /><br />";
-echo "<select><option>Neue function anlegen</option></select><input type='submit' value='>>>'/><br /><br />";
-echo "</body></html>";
+echo "<form action='' method='GET'>";
+echo "<select name='action'><option value='addnewclass'>Neue class anlegen</option></select>";
+echo "<input type ='hidden' name='type' value='class' />";
+echo "<input type='submit' value='>>>' /><br /><br /></form>";
+echo "<form action='?type=function' method='GET'>";
+echo "<select name='action'><option value='addnewfunction'>Neue function anlegen</option></select><input type='submit' value='>>>'/><br /><br />";
+echo "<input type ='hidden' name='type' value='function' />";
+echo "</form></body></html>";
 ?>
